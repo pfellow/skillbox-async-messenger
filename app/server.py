9cc43381ec4d6f6c214b7a6228529ad7cbf7bc14
pfellow <pfellow@gmail.com>
2020-05-07 23:36:00 +0300

    Homework

--- app/server.py
+++ app/server.py
@@ -21,16 +21,26 @@ class ClientProtocol(asyncio.Protocol):
         if self.login is None:
             # login:User
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace("\r\n", "")
-                self.transport.write(
-                    f"Привет, {self.login}!".encode()
-                )
+                temp = decoded.replace("login:", "").replace("\r\n", "")
+                for client in self.server.clients:
+                    if client.login == temp:
+                        self.transport.write(
+                            f"Логин {temp} занят, попробуйте другой".encode()
+                        )
+                        break
+                else:
+                    self.login = temp
+                    self.transport.write(
+                        f"Привет, {self.login}!".encode()
+                    )
+                    self.send_history()
         else:
             self.send_message(decoded)
 
     def send_message(self, message):
         format_string = f"<{self.login}> {message}"
         encoded = format_string.encode()
+        self.server.write_history(encoded)
 
         for client in self.server.clients:
             if client.login != self.login:
@@ -45,15 +55,31 @@ class ClientProtocol(asyncio.Protocol):
         self.server.clients.remove(self)
         print("Соединение разорвано")
 
+    def send_history(self):
+        self.transport.write("Последние сообщения:\n".encode())
+        if len(self.server.messages) == 0:
+            self.transport.write("Отсутствуют".encode())
+        elif len(self.server.messages) < 10:
+            for message in self.server.messages:
+                self.transport.write(message + "\n".encode())
+        else:
+            for i in range(-10, 0):
+                self.transport.write(self.server.messages[i] + "\n".encode())
+
 
 class Server:
     clients: list
+    messages: list
 
     def __init__(self):
         self.clients = []
+        self.messages = []
 
     def create_protocol(self):
         return ClientProtocol(self)
+
+    def write_history(self, message):
+        self.messages.append(message)
 
     async def start(self):
         loop = asyncio.get_running_loop()
